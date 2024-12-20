d8463bbd61c5c421751dd9f14bbb8d272db89be9
felopater-adel2000 <felopater.adel@tera.ltd>
2024-12-20 11:37:12 +0200

    Lec26 Add Login and Home Module

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-//    alias(libs.plugins.android.application) apply false
-//    alias(libs.plugins.kotlin.android) apply false
-    alias(libs.plugins.kotlin.compose) apply false
+    id("org.jetbrains.kotlin.plugin.compose") apply false version "2.0.21"
+//    id("com.android.library") version "8.7.3" apply false
+//    id("org.jetbrains.kotlin.android") version "1.9.24" apply false
 }
