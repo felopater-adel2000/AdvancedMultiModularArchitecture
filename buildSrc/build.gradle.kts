7296d539f6a9dc74dced350ec71d0c72671ce9ff
felopater-adel2000 <felopater.adel@tera.ltd>
2024-12-22 12:07:26 +0200

    Lec39 Test Spotless and Prettier

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -12,4 +12,5 @@ dependencies {
     api(kotlin("gradle-plugin:2.1.0"))
     implementation("com.android.tools.build:gradle:8.7.3")
     implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:2.1.0")
+    implementation("com.diffplug.spotless:spotless-plugin-gradle:6.25.0")
 }
